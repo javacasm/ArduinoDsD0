8905212ac0ba88c20f381e81f3689716fa56ff5a
javacasm <javacasm@gmail.com>
2017-12-30 10:08:11 +0100

    comentarios

--- codigo/_7.2.1_-_Boton/_7.2.1_-_Boton.ino
+++ codigo/_7.2.1_-_Boton/_7.2.1_-_Boton.ino
@@ -1,13 +1,30 @@
+/*
+ * Curso de Arduino
+ *
+ * Parpadeo de led sin delay
+ *
+ * Conexiones
+ * LED conectado a pin 13  (Se puede usar el de la placa)
+ *
+ * by JAVacasM
+ *
+ * revised by chute (2017/11/16)
+ *
+ */
 
+/* ==== Defines - Constantes ==== */
 #define PIN_BOTON   2
 #define PIN_LED   13
+/* ==== END Defines ==== */
 
+/* ==== Setup - Configuracion ==== */
 void setup() {
   pinMode(PIN_BOTON, INPUT);
   pinMode(PIN_LED,OUTPUT);
 }
+/* ==== END Setup ==== */
 
-
+/* ==== Loop - Codigo que se repite==== */
 void loop() {
   int iEstadoBoton=digitalRead(PIN_BOTON);
 
@@ -15,3 +32,4 @@ void loop() {
 
   delay(10);
 }
+/* ==== End Loop ==== */
