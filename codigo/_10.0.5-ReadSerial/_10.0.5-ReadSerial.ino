27c6dc49f1879c58c2d347d7c7794c2b78fce5b3
javacasm <javacasm@gmail.com>
2018-03-19 11:49:27 +0100

    add

--- codigo/_10.0.5-ReadSerial/_10.0.5-ReadSerial.ino
+++ codigo/_10.0.5-ReadSerial/_10.0.5-ReadSerial.ino
@@ -1,14 +1,15 @@
-int incomingByte = 0;   // for incoming serial data
-
 void setup() {
-   Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
-}
-void loop() {       // send data only when you receive data:
-   if (Serial.available() > 0) {        // read the incoming byte:
-          incomingByte = Serial.read();
-          // say what you got:
-          Serial.print("I received: ");
-              Serial.println(incomingByte, DEC);
-        }
+   Serial.begin(9600);     // abrimos el puerto a  9600 bps
 }
 
+void loop() {
+    if (Serial.available() > 0) {  // hay caracteres pendientes de recibir
+
+            char caracterRecibido = Serial.read();
+
+            Serial.print("He recibido el caracter: ");
+            Serial.print(caracterRecibido);   // Imprimimos el caracter
+            Serial.print(" con codigo ascii:");
+            Serial.println(int(caracterRecibido)); // Imprimimos su codigo ascii
+    }
+}
